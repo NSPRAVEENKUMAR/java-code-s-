f614cc75ff458fe8d9f0d32ba65fce63f36aca9d
NS Praveen Kumar <praveenkumarn205@gmail.com>
2025-02-07 12:00:27 +0530

    Add files via upload

---
+++ Day3-Pattern/src/t.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class t {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int sp = n-i; sp >0; sp--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
