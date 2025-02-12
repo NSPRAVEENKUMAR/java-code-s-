fde856f94335fe4716b7e85f59130193743590fa
NS Praveen Kumar <praveenkumarn205@gmail.com>
2025-02-12 16:19:52 +0530

    Add files via upload

---
+++ Binary_tree/b_tree.java
@@ -0,0 +1,73 @@
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    Node root;
+
+    Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        }
+        if (data < root.data) {
+            root.left = insert(root.left, data);
+        } else {
+            root.right = insert(root.right, data);
+        }
+        return root;
+    }
+
+    void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    void postorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+}
+
+public class b_tree {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+
+        System.out.println("In-order Traversal");
+        tree.inorder(tree.root);
+        System.out.println();
+
+        System.out.println("Pre-order Traversal");
+        tree.preorder(tree.root);
+        System.out.println();
+
+        System.out.println("Post-order Traversal");
+        tree.postorder(tree.root);
+    }
+}
